032536776b1107b41ed69df4b947fe5cab9f2ca1
Célio <celiofagundes30@gmail.com>
2022-07-04 10:39:42 -0300

    feat: added department details page

--- pages/departments/[id]/index.tsx
+++ pages/departments/[id]/index.tsx
@@ -1,103 +1,88 @@
-import Image from 'next/image'
-import Seo from '../../../components/Seo'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import useSWR from 'swr'
 import { fetcher } from '../../../lib/fetcher'
-import Layout from '../../../components/Layout'
-import { BackButton } from '../../../components/Navigation/'
-import { Card, CardIdentification, CardPhone, CardWithLabel } from '../../../components/Cards'
+import { Down, Up } from '../../../components/Icons'
 
-import {
-  PageTitleWrapper,
-  Content,
-  UserContainer,
-  UserData,
-  Username,
-  Email,
-  CardsWrapper,
-  SectionOrganizationalData,
-  UserImage,
-  OrganizationalCardsWrapper,
-  OrganizationalCardsRow,
-} from '../../../styles/agents/details'
+import Seo from '../../../components/Seo'
+import Layout from '../../../components/Layout'
+import { Table } from '../../../components/Tables'
+import { BackButton, NavigateButton } from '../../../components/Navigation/'
+
+import { SortSelect, SortButton } from '../../../components/Sorting/'
+import { Content, DropdownIcon, Value, Label, PageTitleWrapper } from '../../../styles/agents'
 import { PageTitle, SectionTitle } from '../../../styles/texts'
-import { User, Calendar } from '../../../components/Icons'
+import axios from 'axios'
+import { Department } from '../../../types/department'
 import { useRouter } from 'next/router'
-import { AgentDetails } from '../../../types/agent'
+import { CardWithLabel } from '../../../components/Cards'
 
 interface DataProps {
-  agent: AgentDetails
+  results: {
+    docs: Department[]
+    hasNextPage: boolean
+    hasPrevPage: boolean
+    limit: number
+    nextPage: boolean
+    page: number
+    pagingCounter: number
+    prevPage: number
+    totalDocs: number
+    totalPages: number
+  }
   success: boolean
 }
-const Agent: React.FC = () => {
+
+interface IsOpenList {
+  [key: string]: boolean
+}
+interface DepartmentData {
+  department: Department
+  success: boolean
+}
+const DepartmentsDetails: React.FC = ({}) => {
   const router = useRouter()
-  const { data, error } = useSWR<DataProps>(
-    router.query.id ? `http://localhost:3000/agents/${router.query.id}` : null,
+  const { data, error } = useSWR<DepartmentData>(
+    router.query.id ? `http://localhost:3000/departments/${router.query.id}` : null,
     fetcher
   )
-
   return (
     <>
-      <Seo title='Detalhe do colaborador' description='Detalhes do colaborador' />
+      <Seo title='Detalhes do Departamentos' description='Detalhes do Departamentos' />
       <Layout>
         <PageTitleWrapper>
-          <BackButton url='/agents' />
-          <PageTitle>Detalhes do colaborador</PageTitle>
+          <BackButton url='/departments' />
+          <PageTitle>Detalhes do departamento</PageTitle>
         </PageTitleWrapper>
-        {data && (
-          <Content>
-            <UserContainer>
-              <UserImage>
-                {data.agent.image ? (
-                  <Image
-                    src={'https://dummyimage.com/80x80/000/fff'}
-                    layout='fixed'
-                    height={80}
-                    width={80}
-                    objectFit='cover'
-                    alt='avatar'
-                    style={{ borderRadius: '50%' }}
-                  />
-                ) : (
-                  <User />
-                )}
-              </UserImage>
+        <Content>
+          {data && <CardWithLabel data={data?.department.name} dataTitle='Departamento' />}
 
-              <UserData>
-                <Username>{data.agent.name}</Username>
-                <Email>{data.agent.email}</Email>
-              </UserData>
-            </UserContainer>
-            <SectionTitle>Informações pessoais</SectionTitle>
-            <CardsWrapper>
-              <CardIdentification data={data.agent.identification} />
-              <CardPhone data={data.agent.phones} />
-              <Card
-                Icon={Calendar}
-                dataTitle='Nascimento'
-                data={new Date(data.agent.birth_date).toLocaleDateString()}
-              />
-            </CardsWrapper>
-            <SectionOrganizationalData>
-              <SectionTitle>Dados Organizacionais</SectionTitle>
-              <OrganizationalCardsWrapper>
-                <OrganizationalCardsRow>
-                  <CardWithLabel dataTitle='Departmento' data={data.agent.department} />
-                  <CardWithLabel dataTitle='Cargo' data={data.agent.role} />
-                </OrganizationalCardsRow>
-                <OrganizationalCardsRow>
-                  <CardWithLabel dataTitle='Unidade' data={data.agent.branch} />
-                  <CardWithLabel
-                    dataTitle='Status'
-                    data={data.agent.status === 'active' ? 'Ativo' : 'Inativo'}
-                  />
-                </OrganizationalCardsRow>
-              </OrganizationalCardsWrapper>
-            </SectionOrganizationalData>
-          </Content>
-        )}
+          <SectionTitle>Listagem das unidades</SectionTitle>
+          {(!data || data.department.branches.length < 1) && <p>Nenhuma unidade encontrado</p>}
+          {data && data.department.branches.length > 0 && (
+            <>
+              <Table>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.Th>Nome da unidade</Table.Th>
+                    <Table.Th></Table.Th>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  {data.department.branches.map(branch => (
+                    <Table.Row key={branch}>
+                      <Table.Td>
+                        <Value>{branch}</Value>
+                      </Table.Td>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+            </>
+          )}
+        </Content>
       </Layout>
     </>
   )
 }
 
-export default Agent
+export default DepartmentsDetails
